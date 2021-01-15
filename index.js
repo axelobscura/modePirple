7006a54db5eadb454f39fd067f9a789b51660bbd
Axel <axosar@gmail.com>
2021-01-14 18:35:23 -0600

    Get the method

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ var server = http.createServer(function(req, res){
   var trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
   //get the http method
+  var method = req.method.toLocaleLowerCase();
 
   //send the response
   res.end('Hello world!');
   //log the request path
-  console.log('Trimmed path is: '+trimmedPath);
+  console.log('Trimmed path is: '+trimmedPath+' with a method: '+method);
 });
-
+//start the server, and have it listen on port 7575
 server.listen(7575, function(){
   console.log('server listening on 7575');
 })

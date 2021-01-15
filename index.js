bcf31b80998880aa7c94706bbe77416b2201f64a
Axel <axosar@gmail.com>
2021-01-14 18:57:56 -0600

    Getting the payload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // Dependencies
 const http = require('http');
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res){
@@ -18,10 +19,27 @@ var server = http.createServer(function(req, res){
   //get the http method
   var method = req.method.toLocaleLowerCase();
 
-  //send the response
-  res.end('Hello world!');
-  //log the request path
-  console.log('Trimmed path is: '+trimmedPath+' with a method: '+method+' with this query string params '+JSON.stringify(queryStringObject));
+  //get the headers as an object
+  var headers = req.headers;
+
+  //get the payload if there is any
+  var decoder = new StringDecoder('utf-8');
+  var buffer = "";
+  req.on('data',function(data){
+    buffer += decoder.write(data);
+  });
+  req.on('end',function(){
+    buffer += decoder.end();
+
+    //send the response
+    res.end('Hello world!');
+    //log the request path
+    //console.log('Trimmed path is: '+trimmedPath+' with a method: '+method+' with this query string params '+JSON.stringify(queryStringObject)+', the headers are '+JSON.stringify(headers));
+    console.log('Request received with this payload: ' + buffer);
+  })
+
+  
+
 });
 //start the server, and have it listen on port 7575
 server.listen(7575, function(){

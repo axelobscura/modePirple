52819c2f670f2f0620374c00e93c5ec314a44cc0
Axel <axosar@gmail.com>
2021-01-14 18:41:38 -0600

    Getting query params

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ var server = http.createServer(function(req, res){
   var path = parsedUrl.pathname;
   var trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
+  //get the query string as an object
+  var queryStringObject = parsedUrl.query;
+
   //get the http method
   var method = req.method.toLocaleLowerCase();
 
   //send the response
   res.end('Hello world!');
   //log the request path
-  console.log('Trimmed path is: '+trimmedPath+' with a method: '+method);
+  console.log('Trimmed path is: '+trimmedPath+' with a method: '+method+' with this query string params '+JSON.stringify(queryStringObject));
 });
 //start the server, and have it listen on port 7575
 server.listen(7575, function(){

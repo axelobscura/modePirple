f460f258edec88984d2fbe266f111c3a081a1a0a
Axel <axosar@gmail.com>
2021-01-25 14:12:14 -0600

    Ping route

--- index.js
+++ index.js
@@ -91,11 +91,10 @@ var unifiedServer = function(req, res){
 // Define the handlers
 var handlers = {};
 
-// Sample handler
-handlers.sample = function(data, callback){
-  // Callback a http status code, and a payload object
-  callback(406, {'name':'sample handler'});
-};
+//Ping Handler
+handlers.ping = function(data,callback){
+  callback(200)
+}
 
 // Not found handler
 handlers.notFound = function(data, callback){
@@ -104,5 +103,5 @@ handlers.notFound = function(data, callback){
 
 // Define a request router
 var router = {
-  'sample': handlers.sample
+  'ping': handlers.ping
 };

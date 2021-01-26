08f0cb2ef0ad9e99b4354cfc806500bff8b370dd
Axel <axosar@gmail.com>
2021-01-26 10:15:39 -0600

    Create file and store json data

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
 const config = require('./config');
 var fs = require("fs");
+var _data = require('./lib/data');
+
+// TESTING
+// @TODO delete this
+_data.create('test','newFile',{'foo' : 'bar'},function(err){
+  console.log('This was the error', err);
+});
 
 // The server should respond to all requests with a string
 var httpServer = http.createServer(function(req, res){
